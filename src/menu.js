b8175914004f2fc333702af574bbf20cd2423887
LebsterFace <44923609+LebsterFace@users.noreply.github.com>
2020-09-11 23:20:48 +0100

    Custom starting page option + start pages from 0 (#4)
    Co-authored-by: Juby210 <31005896+Juby210@users.noreply.github.com>

--- src/menu.js
+++ src/menu.js
@@ -4,38 +4,36 @@ module.exports = class Menu {
     static defaultReactions = { first: '⏪', back: '◀', next: '▶', last: '⏩', stop: '⏹' }
 
     constructor(opts = {}) {
-        const { channel = new TextChannel, userID, pages = [], time = 120000, reactions = Menu.defaultReactions, customCatch = console.error } = opts
+        const { channel = new TextChannel, userID, pages = [], page = 0, time = 120000, reactions = Menu.defaultReactions, customCatch = console.error } = opts
         this.channel = channel
         this.pages = pages
         this.time = time
         this.reactions = reactions
-        this.page = 1
+        this.page = page
         this.catch = customCatch
-        channel.send(pages[0]).then(msg => {
+        channel.send(pages[page]).then(msg => {
             this.msg = msg
             this.addReactions()
             this.createCollector(userID)
         }).catch(customCatch)
     }
-    select(pg = 1) {
+    select(pg = 0) {
         this.page = pg
-        this.msg.edit(this.pages[pg - 1]).catch(this.catch)
+        this.msg.edit(this.pages[pg]).catch(this.catch)
     }
     createCollector(uid) {
         const collector = this.msg.createReactionCollector((r, u) => u.id == uid, { time: this.time })
         this.collector = collector;
         collector.on('collect', r => {
-            if(r.emoji.name == this.reactions.first) {
-                if(this.page != 1) this.select(1);
-            } else if(r.emoji.name == this.reactions.back) {
-                if(this.page != 1) this.select(this.page - 1);
-            } else if(r.emoji.name == this.reactions.next) {
-                if(this.page != this.pages.length) this.select(this.page + 1);
-            } else if(r.emoji.name == this.reactions.last) {
-                if(this.page != this.pages.length) this.select(this.pages.length);
-            } else if(r.emoji.name == this.reactions.stop) {
-                collector.stop();
-            }
+            if (r.emoji.name == this.reactions.first) {
+                if (this.page != 0) this.select(0)
+            } else if (r.emoji.name == this.reactions.back) {
+                if (this.page != 0) this.select(this.page - 1)
+            } else if (r.emoji.name == this.reactions.next) {
+                if (this.page < this.pages.length) this.select(this.page + 1)
+            } else if (r.emoji.name == this.reactions.last) {
+                if (this.page != this.pages.length) this.select(this.pages.length - 1)
+            } else if (r.emoji.name == this.reactions.stop) collector.stop()
             r.users.remove(uid).catch(this.catch)
         })
         collector.on('end', () => {
